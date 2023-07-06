1f194fc9d40d9f95df8a2802bfc6f4c0bfa93805
simonsruggi <simone.ruggiero97@gmail.com>
2023-07-06 23:03:54 +0200

    new restocks website

--- paymentchecker.py
+++ paymentchecker.py
@@ -1,11 +1,9 @@
 import requests
-import re
-from bs4 import BeautifulSoup
-import json
 from datetime import datetime as dt2
 import sys
 from dotenv import load_dotenv
 import os
+import numpy as np
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -24,53 +22,62 @@ if email is None or password is None:
     raise ValueError('Email or password environment variables are not set.')
 
 
-restocks_headers = {
-    'Host': 'restocks.net',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+api_restocks_headers = {
+    'Host': 'api.restocks.net',
+    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+    'restocks-valuta': 'EUR',
+    'restocks-platform': 'web',
+    'accept-language': 'it',
+    'sec-ch-ua-mobile': '?0',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    'content-type': 'application/json',
+    'restocks-country': 'NL',
+    'sec-ch-ua-platform': '"macOS"',
+    'accept': '*/*',
+    'origin': 'https://restocks.net',
+    'sec-fetch-site': 'same-site',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://restocks.net/',
 }
 
 
 def login():
 
+    global authentication_token
+
     session = requests.Session()
 
-    session.headers.update(restocks_headers)
-
-    print('Logging in...')
-
-    response = session.get('https://restocks.net/it/login')
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    csrf_token = soup.find('meta', {'name': 'csrf-token'})['content']
-
-    data = {'_token': csrf_token,
-            'email': email,
-            'password': password
-            }
-
-    response = session.post('https://restocks.net/it/login', data=data)
-
-    response = session.get('https://restocks.net/it/account/profile')
-
-    soup = BeautifulSoup(response.text, 'html.parser')
+    session.headers.update(api_restocks_headers)
 
     try:
 
-        account_name = soup.find(
-            'div', {'class': 'col-lg-3 col-md-4'}).findAll('span')[1].text
+        print('Logging in...')
 
-        print('Successfully logged in as {}'.format(account_name))
+        data = {
+            'email': email,
+            'password': password
+        }
 
-    except:
+        response = session.post(
+            'https://api.restocks.net/v1/auth/login', json=data)
 
-        print('Login failed')
+        account_name = response.json()['data']['user']['firstname']
+
+        authentication_token = response.json()['data']['token']
+
+        print('Logged in successfully as {}'.format(account_name))
+
+        session.headers.update(
+            {'authorization': 'Bearer {}'.format(authentication_token)})
+
+        return session
+
+    except Exception as e:
+
+        print('Login failed, closing app')
 
         sys.exit()
-
-
-    return session
 
 
 def payment_mode(session):
@@ -86,79 +93,77 @@ def payment_mode(session):
 
         paid_ids = []
 
-    def scraping_sales(session):
-
-        raw_sales_data = ''
-
-        page = 1
-
-        while True:
-            print('Getting sales page: {}'.format(page))
-            response = session.get(
-                f"https://restocks.net/it/account/sales/history?page={page}")
-            if "no__listings__notice" in response.text:
-                break
-            else:
-                formatted_str = re.sub(r'\\n', '', response.text)
-                raw_sales_data += json.loads(formatted_str)["products"]
-                page += 1
-
-        return raw_sales_data
-
-    def preprocess_data(raw_sales_data):
-        raw_sales_data = re.sub(r'\\', '', raw_sales_data)
-        raw_sales_data = re.sub(r"<br/>", "", raw_sales_data)
-        raw_sales_data = re.sub(r"<span>", "", raw_sales_data)
-        raw_sales_data = re.sub(r"</span>", "", raw_sales_data)
-        raw_sales_data = re.sub(r"     ", "", raw_sales_data)
-        raw_sales_data = re.sub(r"                ", " ", raw_sales_data)
-        preprocessed_data = re.sub(r"            ", "", raw_sales_data)
-        all_sales = BeautifulSoup(
-            preprocessed_data, "html.parser").findAll("tr")
-        return all_sales
-
-    sales = preprocess_data(scraping_sales(session))
+    page = 1
+    sales_finished = False
     json_sales = []
-    for sale in sales[1:]:
-        json_sale = {}
-        sale = str(sale)
-        sale = sale.replace('<td>', "pause")
-        sale = sale.replace('</td>', "pause")
-        sale = sale.replace('<br/>', "pause")
+    while not sales_finished:
 
-        parsed_sale = [i for i in sale.split("pause") if len(i) > 1]
+        print('Getting sales page: {}'.format(page))
+        session.headers.update(api_restocks_headers)
 
-        item = parsed_sale[2].strip()
-        sale_id = parsed_sale[3].strip().replace('ID: ', '')
-        price = float((parsed_sale[4].replace("€", "").strip()))
-        date = parsed_sale[5].strip()
+        response = session.get(
+            f"https://api.restocks.net/v1/shop/account/sales/history?page={page}")
+        for i in response.json()['data']:
 
-        json_sale['item'] = item
-        json_sale['sale_id'] = sale_id
-        json_sale['price'] = price
-        json_sale['date'] = date
+            json_sale = {}
+            json_sale['sale_id'] = str(i['id'])
+            json_sale['price'] = i['payout'].replace('€ ', '')
+            json_sale['date'] = i['date']
+            json_sale['item'] = i['baseproduct']['name'] + \
+                ' ' + str(i['size']['name'])
+            json_sales.append(json_sale)
 
-        json_sales.append(json_sale)
+        if not response.json()['data']:
+            print('Sales finished')
+            sales_finished = True
+        else:
+            page += 1
 
     for a in sorted(json_sales, key=lambda x: dt2.strptime(x['date'], '%d/%m/%y')):
 
         if a['sale_id'] not in paid_ids:
 
-            print('{} Missing payment for price {} and product {} with id {}'.format(a['date'], a['price'], a['item'], a['sale_id']))
-
+            start = dt2.strptime(a['date'], '%d/%m/%y').date()
+            end = dt2.today().date()
+            business_days = np.busday_count(start, end)
+            if business_days >= 39:
+                print('{} Missing payment for price {} and product {} with id {}, business days passed: {}'.format(
+                    a['date'], a['price'], a['item'], a['sale_id'], business_days))
+            elif business_days >= 25 and business_days <= 38:
+                print('{} Missing payment for price {} and product {} with id {}, business days passed: {}'.format(
+                    a['date'], a['price'], a['item'], a['sale_id'], business_days))
+            else:
+                print('{} Missing payment for price {} and product {} with id {}, business days passed: {}'.format(
+                    a['date'], a['price'], a['item'], a['sale_id'], business_days))
         else:
-
             json_sales.remove(a)
 
-    print('Total missing {} €'.format(int(sum(a['price'] for a in json_sales))))
+    print('Total missing {}€ for a total of {} sales '.format(
+        int(sum(float(a['price']) for a in json_sales)), len(json_sales)))
 
     add_payment = False
-    
-    print('Insert "a" if you want to add a paid payment or press ENTER to close')
-    
-    if input() == 'a':
+
+    print('Insert "a" if you want to add a paid payment or insert "r" to get the restocks email version or press ENTER to close')
+
+    new_choice = input()
+
+    if new_choice == 'a':
 
         add_payment = True
+
+    elif new_choice == 'r':
+
+        for a in sorted(json_sales, key=lambda x: dt2.strptime(x['date'], '%d/%m/%y')):
+
+            if a['sale_id'] not in paid_ids:
+
+                start = dt2.strptime(a['date'], '%d/%m/%y').date()
+                end = dt2.today().date()
+                business_days = np.busday_count(start, end)
+                if business_days >= 39:
+
+                    print('{} Missing payment for price {} and product {} with id {}, business days passed: {}'.format(
+                        a['date'], a['price'], a['item'], a['sale_id'], business_days))
 
     while add_payment:
 
@@ -170,17 +175,19 @@ def payment_mode(session):
 
             add_payment = False
 
+            break
+
         if paid_id in paid_ids:
 
             print('Payment already added to list')
 
         else:
-
             for s in json_sales:
                 if str(s['sale_id']) == str(paid_id):
 
-                    print('Congrats! You got your payment of {} for product {} and id {} sold on {}'.format(s['price'],s['item'],s['sale_id'],s['date']))
-            
+                    print('Congrats! You got your payment of {} for product {} and id {} sold on {}'.format(
+                        s['price'], s['item'], s['sale_id'], s['date']))
+
                     break
             try:
                 with open(sales_txt_path, 'a') as f:
@@ -188,8 +195,7 @@ def payment_mode(session):
                     f.writelines('\n'+paid_id)
 
             except Exception as e:
-                print('Error', e )
-
+                print('Error', e)
 
 
 def main():
